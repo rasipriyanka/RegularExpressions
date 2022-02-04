6998562ac916d570ad1af6e05073049896be165c
priyanka <priyankapendela111@gmail.com>
2022-02-04 14:43:13 +0530

    [Priyanka]Added the validation for first name in regex

---
+++ RegularExpression/RegularExpression.cs
@@ -0,0 +1,29 @@
+﻿//-------Dependencies-----------//
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+//------Namespace----------//
+namespace RegularExpression
+{
+    //--------Class-------//
+    internal class RegularExpression
+    {
+        public static string FirstName = "^[A-Z][A-Z a-z]{3,}$";
+        //  method is used for validating first name
+        //----------Method Starts------------//
+        public void ValidateFirstName(string firstName)
+        {
+            if (Regex.IsMatch(firstName, FirstName))
+            {
+                Console.WriteLine("{0} is valid", firstName);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not valid", firstName);
+            }
+            //----------Method Ends-------------//
+        }
+    }
+}
+
