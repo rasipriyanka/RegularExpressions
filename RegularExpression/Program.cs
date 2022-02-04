41d587878f721271ef647f5d89edb3dc979a8459
priyanka <priyankapendela111@gmail.com>
2022-02-04 15:39:02 +0530

    [Priyanka]Added the implementation For validating password minimum characters

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -16,6 +16,8 @@ namespace RegularExpression
             regularExpressions1.ValidateEmailAddress("rasi.priyanka@bridgelabz.com");
             RegularExpression regularExpressions3 = new RegularExpression();
             regularExpressions3.ValidateMobileNum("+91 9326547896");
+            RegularExpression regularExpressions4 = new RegularExpression();
+            regularExpressions4.ValidatePassWord("Rasi5678");
 
         }
     }
--- RegularExpression/RegularExpression.cs
+++ RegularExpression/RegularExpression.cs
@@ -43,9 +43,9 @@ namespace RegularExpression
                 Console.WriteLine("{0} is not valid", lastName);
             }
         }
-            //----------Method Ends here-------------//
+        //----------Method Ends here-------------//
 
-            // Validating Email
+        // Validating Email
         public static string EmailAddress = @"^([A-Za-z0-9]*\.[A-Za-z0-9]*)@([A-Za-z0-9]*)((\.(\w){2,3})+)$";
         //----------Method Starts Here------------//
         public void ValidateEmailAddress(string email)//-Validating the EmailAddres
@@ -67,7 +67,7 @@ namespace RegularExpression
         public static string MobileNum = @"^([\+][0-9]{2}\s*[0-9]{10})$";
         //----------Method Starts Here------------//
         // this method used for the validating mobile number
-        public void ValidateMobileNum(string mobile) 
+        public void ValidateMobileNum(string mobile)
         {
             if (Regex.IsMatch(mobile, MobileNum))
             {
@@ -79,8 +79,26 @@ namespace RegularExpression
             }
             //----------Method Ends here-------------//
         }
+
+        //----------Validating Password-------//
+        public static string Password = @"^[A-Za-z0-9]{8,}$";
+        //----------Method Starts Here------------//
+        // this method used for the validating password
+        public void ValidatePassWord(string password)
+        {
+            if (Regex.IsMatch(password, Password))
+            {
+                Console.WriteLine("{0} is valid", password);
+                Console.WriteLine("Password :", password);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not valid", password);
+            }
+            //----------Method Ends here-------------//
+        }
+
     }
-    
 }
 
 
